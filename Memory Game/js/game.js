6637fe651eedcfdc3cec70bd053e8b59875ade56
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-10-18 16:25:39 -0300

    Editando e adicionando arquivos

---
+++ Memory Game/js/game.js
@@ -0,0 +1,32 @@
+const grid = document.querySelector('.grid');
+
+const cards = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy'
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+const createCard = () => {
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+}
+
